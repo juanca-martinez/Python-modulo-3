1aafb64fae5b09277cd5e541419ee14587d67d07
jota kam <jcm.matus@gmail.com>
2023-05-06 11:43:45 -0400

    creacion de listas y separacion de estas

--- Modulo3.py
+++ Modulo3.py
@@ -1,7 +1,7 @@
 Listado = [
     "Harry Houdini",
     "Newton",
-    "David Blaine ",
+    "David Blaine",
     "Hawking",
     "Messi",
     "Teller",
@@ -9,4 +9,15 @@ Listado = [
     "Pele",
     "Juanes",
 ]
+magos = []
+cientificos = []
+Otros = []
 
+for lista in Listado:
+    print(lista)
+    if lista == 'Harry Houdini' or lista == "David Blaine" or lista == "Teller":
+        print(lista + " es el mejor mago de todos los tiempos")
+    elif lista == 'Newton' or lista == "Hawking" or lista == "Einstein":
+        print(lista + " es el mejor cientifico de todos los tiempos")
+    else:
+        print(lista + " es el mejor deportista de todos los tiempos")
